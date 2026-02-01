42a62d214075b4c369cc31df47c48fd5faffb86d
Hung-Nguyen-dev <hungnguyenmauphi123@gmail.com>
2026-02-01 20:32:42 +0700

    init project

---
+++ src/main/java/com/mobilebackend/ungdunglapkehoachdulich/UngdunglapkehoachdulichApplication.java
@@ -0,0 +1,13 @@
+package com.mobilebackend.ungdunglapkehoachdulich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UngdunglapkehoachdulichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UngdunglapkehoachdulichApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mobilebackend/ungdunglapkehoachdulich/UngdunglapkehoachdulichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilebackend.ungdunglapkehoachdulich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UngdunglapkehoachdulichApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
